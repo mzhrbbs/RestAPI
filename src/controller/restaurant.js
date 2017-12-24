8dcbebe0281aa41405f8c4ec99f64a3866e07051
mazhar <mzhrbbs@gmail.com>
2017-12-24 14:04:33 -0800

    Added basic CRUD

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,7 +5,9 @@ import Restaurant from '../models/restaurant';
 export default ({config,db}) => {
   let api = Router();
 
-  // '/v1/restaurant/add'
+  //CRUD - Create , Read , Update , Delete
+
+  // '/v1/restaurant/add'-- Create
   api.post('/add',(req,res) => {
     let newRestaurant = new Restaurant();
     newRestaurant.name = req.body.name;
@@ -18,6 +20,53 @@ export default ({config,db}) => {
     });
   });
 
-  return api;
+  // '/v1/restaurant ' -- Read
+  api.get('/', (req,res) => {
+    Restaurant.find({},(err, restaurants) =>{
+      if (err){
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
 
+  // 'v1/restaurant/:id'  -- Read one
+  api.get('/:id', (req,res) => {
+    Restaurant.findById(req.params.id, (err,restaurant) => {
+      if (err){
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  // 'v1/restaurant/:id' -- Update
+  api.put('/:id', (req,res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err){
+        res.send(err);
+      }
+      restaurant.name = req.body.name;
+      restaurant.save( (err) => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({message: "Restaurant info udpated !"});
+      });
+    });
+  });
+
+  // 'v1/restaurant/:id ' -- Delete
+  api.delete('/:id' , (req,res) => {
+    Restaurant.remove({
+      _id: req.params.id
+    },(err,restaurant) => {
+      if (err){
+        res.send(err);
+      }
+      res.json({message: "Restaurant deleted successfully !"});
+    });
+  });  
+
+  return api;
 }
